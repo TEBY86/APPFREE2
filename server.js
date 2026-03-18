b8060016675b12ccbf38cca030b67e7ce22449ed
sebastian <sebaleivavega@gmail.com>
2026-03-17 21:11:27 -0300

    fix: server solo sirve index como entrada

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
 app.use(express.static('.'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.listen(process.env.PORT || 3000);
