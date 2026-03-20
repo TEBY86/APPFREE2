e831ba5cc3f6b99426140344f0256c6fae4ff648
sebastian <sebaleivavega@gmail.com>
2026-03-20 20:43:57 -0300

    fix: pasar

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('.'));
+// Usar __dirname garantiza que encuentre la carpeta 'imagenes'
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(process.env.PORT || 3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
